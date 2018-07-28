583c2fcf4e431778a027305c5d88926b02287b4b
Tanya Amberg <tanya_amberg@mail.ru>
2018-07-28 16:48:18 +0300

    start

---
+++ InfoTravelApp/InfoTravelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfoTravelApp
+//
+//  Created by Tanya Amberg on 28.07.2018.
+//  Copyright © 2018 Tanya Amberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
